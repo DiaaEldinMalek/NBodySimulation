bcb52a8f8230677815c89057fc1a952ccc11a55e
DiaaEldinMalek <s-dia2sultan@zewailcity.edu.eg>
2021-08-19 13:22:24 +0200

    Seperated ICPLUM into files

---
+++ PlummerSphereIC.h
@@ -0,0 +1,17 @@
+//
+// Created by dia2s on 8/19/2021.
+//
+
+#ifndef MAIN_CPP_PLUMMERSPHEREIC_H
+#define MAIN_CPP_PLUMMERSPHEREIC_H
+
+#include "fstream"
+#include <iomanip>
+#include "boost/multi_array.hpp"
+//using boost::extents;
+typedef boost::multi_array<long double, 2 > Two_D_Array;
+
+void PlumIC(const int Num, long double ZMass, long double Pmass, long double& TKE, Two_D_Array &PositionsL,Two_D_Array & VeL, Two_D_Array &PositionsS, Two_D_Array &VeS);
+
+
+#endif //MAIN_CPP_PLUMMERSPHEREIC_H
