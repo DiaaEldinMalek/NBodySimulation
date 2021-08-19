8c94071cce9b7cba87b6a3d4956352859c727ab6
DiaaEldinMalek <s-dia2sultan@zewailcity.edu.eg>
2021-08-20 00:04:10 +0200

    Analyser Functions

--- PlummerSphereIC.h
+++ PlummerSphereIC.h
@@ -10,8 +10,8 @@
 #include "boost/multi_array.hpp"
 //using boost::extents;
 typedef boost::multi_array<long double, 2 > Two_D_Array;
-
-void PlumIC(const int seed, const int Num, const long double ZMass, const long double Pmass, long double& TKE, Two_D_Array &PositionsL,Two_D_Array & VeL, Two_D_Array &PositionsS, Two_D_Array &VeS);
+void PlumIC(const int seed, const int Num, const long double ZMass, const long double Pmass, long double& TKE,
+            Two_D_Array &PositionsL,Two_D_Array & VeL, Two_D_Array &PositionsS, Two_D_Array &VeS);
 
 
 #endif //MAIN_CPP_PLUMMERSPHEREIC_H
--- main.cpp
+++ main.cpp
@@ -6,29 +6,34 @@
 #include <iomanip>
 #include "PlummerSphereIC.h"
 #include "CalcAcc.h"
+#include "Analyser.h"
+
+
 using boost::extents;
 typedef boost::multi_array<long double, 2 > Two_D_Array;
 typedef boost::multi_array<long double, 1 > One_D_Array;
 long double pi = (long double) 4.0 *atan(1.0);
 
-
 int main() {
+    int NLeap = 2;
     int CodeTime= time(0);
     const int Num = 1000;
     int seed = 0;
     long double ZMass= 1.0;
+    AnalyserValues ERROR_VALS;
     Two_D_Array PositionL(extents[Num][3]),PositionS(extents[Num][3]);//position vectors of particles
     Two_D_Array VeL(extents[Num][3]), VeS(extents[Num][3]);//velocity vectors of particles (Large ts vs small ts)
     Two_D_Array AccL(extents[Num][3]), AccS(extents[Num][3]);//acceleration vectors of particles
     One_D_Array PEL(extents[Num]), PES(extents[Num]);
     long double TotalPotentialEnergy=0, TotalKineticEnergy=0;
-    long double error_C, eNorm_C, error_V, eNorm_V, error_E, eNorm_E, error_P, eNorm_P;
     long double pMass= (long double) ZMass/Num;
     long double Tend = 100.0;
     long double dT =1.0;
-    int NLeap = 2;
+
     PlumIC(seed, Num, ZMass, pMass, TotalKineticEnergy, PositionL, VeL, PositionS, VeS);
     CalcAcc(Num, pMass, PositionL, AccL, TotalPotentialEnergy, PEL);
+    ERROR_VALS.Analyser(Num, pMass, PositionL, PositionS, VeL, VeS, PEL, PES);
+
     std::cout <<time(0)-CodeTime;
     return 0;
 }
